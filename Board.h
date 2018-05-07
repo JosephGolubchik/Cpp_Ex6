3ad28660914f8cef164a11c765e8f5cf3827e9b9
JosephGolubchik <37150623+JosephGolubchik@users.noreply.github.com>
2018-05-07 19:43:59 +0300

    Update Board.h

--- Board.h
+++ Board.h
@@ -1,14 +1,29 @@
 #include <iostream>
-
+#include "Point.h"
 using namespace std;
 
 class Board {
-    
-    private:
-    int size;
-    char** pBoard;
-    
     public:
-    Board(int size);
+    int size;
+    char* pBoard;
+  //  int x,y;
+  char temp;
     
+    Board(int size):size(size),
+    pBoard(new char [size*size]){
+        operator=('.');
+    }
+    
+    Board(Board& b){
+        size = b.size;
+        pBoard = new char[size*size];
+        *this = b;
+    }
+    
+    Board& operator=(char w);
+    Board& operator=(Board& b);
+    friend ostream& operator<<(ostream& os, Board& b);
+    int getIndex(int x,int y);
+    char& operator[](Point p);
 };
+
