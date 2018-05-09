f923530a230444f46d23bd16f8523e131ecf8fcb
JosephGolubchik <37150623+JosephGolubchik@users.noreply.github.com>
2018-05-09 20:30:07 +0300

    Update Point.h

--- Point.h
+++ Point.h
@@ -1,10 +1,35 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
 class Point{
     public:
     int x;
     int y;
+    char data;
     
     Point(int x,int y):
     x(x),y(y){
     }
     
+    friend ostream& operator<<(ostream& os, Point& p) {  
+         os << p.x << "," << p.y << endl;
+    return os;  
+    }
+    
+    Point &Point::operator=(const char ch){
+        
+        if (ch == '.' || ch == 'X' || ch == 'O')
+            data = ch;
+        else {
+            IllegalCharException ex {ch};
+            throw ex;
+        }
+        return *this;
+    }
 };
+    
+
+
+
+
