bdd4e064af53e24d6f37225c675518bda8349021
JosephGolubchik <37150623+JosephGolubchik@users.noreply.github.com>
2018-05-09 23:58:10 +0300

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -3,86 +3,39 @@
 #include <iostream>
 using namespace std;
 
-
-Board& Board::operator=(const char w){
-    if(w == '.'|| w == 'O' || w == 'X'){
-        for (int i = 0; i < size; i++) {
-            for(int j = 0; j < size; j++){
-                pBoard[i][j].data = w;
-            }
-        }
-    }
-    else{
-        throw IllegalCharException(w);
+Point Point::operator=(const char c){
+    
+    if (c == '.' || c == 'X' || c == 'O')
+        data = c;
+    else {
+        IllegalCharException ex {c};
+        throw ex;
     }
     return *this;
 }
 
-Board& Board::operator=(const Board& b){
-    for (int i = 0; i < size; i++) {
-        for(int j = 0; j < size; j++){
-            pBoard[i][j].data=b.pBoard[i][j].data;
-        }
-    }
+Point& Point::operator=(const Point& p){
+    data = p.data;
     return *this;
 }
 
-bool Board::operator == (Board const & r){
-    if(size != r.size) return false;
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            if(pBoard[i][j] != r.pBoard[i][j]) return false;
-        }
-    }
-    return true;
+Point::operator char(){
+    return data;
 }
 
-
-ostream& operator<<(ostream& os, Board const& b) {  
-      for (int i = 0; i < b.size; i++) {
-        for(int j=0; j<b.size; j++){
-            os << b.pBoard[i][j].data;
-        }
-        os << endl;
-      }
-    return os;  
-}
-
-Point& Board::operator[](Point p){
-    if(p.x < 0 || p.x >= size || p.y < 0 || p.y >= size){
-        throw IllegalCoordinateException(p.x, p.y);
-    }
-    else{
-        return pBoard[p.x][p.y];
-    }
-
-}
-
-const Point& Board::operator[](Point p) const{
-    if(p.x < 0 || p.x >= size || p.y < 0 || p.y >= size){
-        throw IllegalCoordinateException(p.x, p.y);
-    }
-    else{
-        return pBoard[p.x][p.y];
-    }
-
-}
-
-void Board::free(){
-    for(int i = 0; i < size; i++)
-        delete[] pBoard[i];
-    delete[] pBoard;
-}
-
-// void Board::free(){
-//     for(int i = 0; i < size; i++){
-//         delete[] pBoard[i];
+// bool Point::operator==(const char c){
+//     if(data == c){
+//         return data == c;
 //     }
-//     delete[] pBoard;
+//     else{
+//         throw 0;
+//     }
 // }
+// Point::~Point(){}
 
-Board::~Board(){
-	free();
+bool operator == (Point const & l, char const & r){
+    return l.data == r;
 }
-
-
+// char operator= (char c, Point& p){ 
+//     return p.data;
+// }
