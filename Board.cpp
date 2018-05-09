b0447cf88a17da357c11945f56da62bf28fdaf49
JosephGolubchik <37150623+JosephGolubchik@users.noreply.github.com>
2018-05-10 00:00:45 +0300

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -3,39 +3,86 @@
 #include <iostream>
 using namespace std;
 
-Point Point::operator=(const char c){
-    
-    if (c == '.' || c == 'X' || c == 'O')
-        data = c;
-    else {
-        IllegalCharException ex {c};
-        throw ex;
+
+Board& Board::operator=(const char w){
+    if(w == '.'|| w == 'O' || w == 'X'){
+        for (int i = 0; i < size; i++) {
+            for(int j = 0; j < size; j++){
+                pBoard[i][j].data = w;
+            }
+        }
+    }
+    else{
+        throw IllegalCharException(w);
     }
     return *this;
 }
 
-Point& Point::operator=(const Point& p){
-    data = p.data;
+Board& Board::operator=(const Board& b){
+    for (int i = 0; i < size; i++) {
+        for(int j = 0; j < size; j++){
+            pBoard[i][j].data=b.pBoard[i][j].data;
+        }
+    }
     return *this;
 }
 
-Point::operator char(){
-    return data;
+bool Board::operator == (Board const & r){
+    if(size != r.size) return false;
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if(pBoard[i][j] != r.pBoard[i][j]) return false;
+        }
+    }
+    return true;
 }
 
-// bool Point::operator==(const char c){
-//     if(data == c){
-//         return data == c;
-//     }
-//     else{
-//         throw 0;
-//     }
-// }
-// Point::~Point(){}
 
-bool operator == (Point const & l, char const & r){
-    return l.data == r;
+ostream& operator<<(ostream& os, Board const& b) {  
+      for (int i = 0; i < b.size; i++) {
+        for(int j=0; j<b.size; j++){
+            os << b.pBoard[i][j].data;
+        }
+        os << endl;
+      }
+    return os;  
 }
-// char operator= (char c, Point& p){ 
-//     return p.data;
+
+Point& Board::operator[](Point p){
+    if(p.x < 0 || p.x >= size || p.y < 0 || p.y >= size){
+        throw IllegalCoordinateException(p.x, p.y);
+    }
+    else{
+        return pBoard[p.x][p.y];
+    }
+
+}
+
+const Point& Board::operator[](Point p) const{
+    if(p.x < 0 || p.x >= size || p.y < 0 || p.y >= size){
+        throw IllegalCoordinateException(p.x, p.y);
+    }
+    else{
+        return pBoard[p.x][p.y];
+    }
+
+}
+
+void Board::free(){
+    for(int i = 0; i < size; i++)
+        delete[] pBoard[i];
+    delete[] pBoard;
+}
+
+// void Board::free(){
+//     for(int i = 0; i < size; i++){
+//         delete[] pBoard[i];
+//     }
+//     delete[] pBoard;
 // }
+
+Board::~Board(){
+	free();
+}
+
+
